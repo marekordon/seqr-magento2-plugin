f6a043dea8261b961b8dba95b1474c48ad38a65a
marekordon <marek.ordon@seamless.se>
2016-03-24 14:01:22 +0100

    v0.0.1.1

--- Model/Payment.php
+++ Model/Payment.php
@@ -1,210 +1,56 @@
 <?php
 /**
-* SEQR_Plugin default configuration
-*
-* @category    SEQR
-* @package     SEQR_Plugin
-* @author      Marek Ordon
-* @copyright   SEQR Poland
-* @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
  */
+namespace SEQR\Plugin\Model;
 
-namespace Seqr\Plugin\Model;
 
-class Payment extends \Magento\Payment\Model\Method\Cc
+
+/**
+ * Pay In Store payment method model
+ */
+class Payment extends \Magento\Payment\Model\Method\AbstractMethod
 {
-    const CODE = 'seqr_plugin';
-
-    protected $_code = self::CODE;
-
-    protected $_isGateway                   = true;
-    protected $_canCapture                  = true;
-    protected $_canCapturePartial           = true;
-    protected $_canRefund                   = true;
-    protected $_canRefundInvoicePartial     = true;
-
-    protected $_stripeApi = false;
-
-    protected $_countryFactory;
-
-    protected $_minAmount = null;
-    protected $_maxAmount = null;
-    protected $_supportedCurrencyCodes = array('USD');
-
-    protected $_debugReplacePrivateDataKeys = ['number', 'exp_month', 'exp_year', 'cvc'];
-
-    public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
-        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
-        \Magento\Payment\Helper\Data $paymentData,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Payment\Model\Method\Logger $logger,
-        \Magento\Framework\Module\ModuleListInterface $moduleList,
-        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
-        \Magento\Directory\Model\CountryFactory $countryFactory,
-//         \Stripe\Stripe $stripe,
-        array $data = array()
-    ) {
-        parent::__construct(
-            $context,
-            $registry,
-            $extensionFactory,
-            $customAttributeFactory,
-            $paymentData,
-            $scopeConfig,
-            $logger,
-            $moduleList,
-            $localeDate,
-            null,
-            null,
-            $data
-        );
-
-        $this->_countryFactory = $countryFactory;
-
-//         $this->_stripeApi = $stripe;
-//         $this->_stripeApi->setApiKey(
-//             $this->getConfigData('api_key')
-//         );
-
-        $this->_minAmount = $this->getConfigData('min_order_total');
-        $this->_maxAmount = $this->getConfigData('max_order_total');
-    }
 
     /**
-     * Payment capturing
+     * Payment code
      *
-     * @param \Magento\Payment\Model\InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws \Magento\Framework\Validator\Exception
+     * @var string
      */
-    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        //throw new \Magento\Framework\Validator\Exception(__('Inside Stripe, throwing donuts :]'));
-    	$this->_logger->debug(__('CAPTURE!!!!!!!!!!'));
-
-        /** @var \Magento\Sales\Model\Order $order */
-        $order = $payment->getOrder();
-
-        /** @var \Magento\Sales\Model\Order\Address $billing */
-        $billing = $order->getBillingAddress();
-
-        try {
-            $requestData = [
-                'amount'        => $amount * 100,
-                'currency'      => strtolower($order->getBaseCurrencyCode()),
-                'description'   => sprintf('#%s, %s', $order->getIncrementId(), $order->getCustomerEmail()),
-                'card'          => [
-                    'number'            => $payment->getCcNumber(),
-                    'exp_month'         => sprintf('%02d',$payment->getCcExpMonth()),
-                    'exp_year'          => $payment->getCcExpYear(),
-                    'cvc'               => $payment->getCcCid(),
-                    'name'              => $billing->getName(),
-                    'address_line1'     => $billing->getStreetLine(1),
-                    'address_line2'     => $billing->getStreetLine(2),
-                    'address_city'      => $billing->getCity(),
-                    'address_zip'       => $billing->getPostcode(),
-                    'address_state'     => $billing->getRegion(),
-                    'address_country'   => $billing->getCountryId(),
-                    // To get full localized country name, use this instead:
-                    // 'address_country'   => $this->_countryFactory->create()->loadByCode($billing->getCountryId())->getName(),
-                ]
-            ];
-
-            $charge = \Stripe\Charge::create($requestData);
-            $payment
-                ->setTransactionId($charge->id)
-                ->setIsTransactionClosed(0);
-
-        } catch (\Exception $e) {
-            $this->debugData(['request' => $requestData, 'exception' => $e->getMessage()]);
-            $this->_logger->error(__('Payment capturing error.'));
-            throw new \Magento\Framework\Validator\Exception(__('Payment capturing error.'));
-        }
-
-        return $this;
-    }
+    protected $_code = 'seqr-payment';
 
     /**
-     * Payment refund
+     * Availability option
      *
-     * @param \Magento\Payment\Model\InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws \Magento\Framework\Validator\Exception
+     * @var bool
      */
-    public function refund(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        $transactionId = $payment->getParentTransactionId();
+    protected $_isOffline = false;
 
-        try {
-            \Stripe\Charge::retrieve($transactionId)->refund();
-        } catch (\Exception $e) {
-            $this->debugData(['transaction_id' => $transactionId, 'exception' => $e->getMessage()]);
-            $this->_logger->error(__('Payment refunding error.'));
-            throw new \Magento\Framework\Validator\Exception(__('Payment refunding error.'));
-        }
-
-        $payment
-            ->setTransactionId($transactionId . '-' . \Magento\Sales\Model\Order\Payment\Transaction::TYPE_REFUND)
-            ->setParentTransactionId($transactionId)
-            ->setIsTransactionClosed(1)
-            ->setShouldCloseParentTransaction(1);
-
-        return $this;
-    }
 
     /**
-     * Determine method availability based on quote amount and config data
+     * Check whether payment method can be used
      *
      * @param \Magento\Quote\Api\Data\CartInterface|null $quote
      * @return bool
      */
     public function isAvailable($quote = null)
     {
-    	
-//         if ($quote && (
-//             $quote->getBaseGrandTotal() < $this->_minAmount
-//             || ($this->_maxAmount && $quote->getBaseGrandTotal() > $this->_maxAmount))
-//         ) {
-//         	$this->_logger->debug(__('return false 1'));
-//             return false;
-//         }
-
-// //         if (!$this->getConfigData('api_key')) {
-// //         	$this->_logger->debug(__('return false 2'));
-// //             return false;
-// //         }
-//         $this->_logger->debug(parent::isAvailable($quote));
-//         return parent::isAvailable($quote);
-		
-    	
-    	$this->_logger->debug($this->getConfigData('soap_url'));
-    	
-    	if($this->getConfigData('active')){
-    		$this->_logger->debug(__('return true'));
-			return true;
-    	} else {
-    		$this->_logger->debug(__('return false'));
-    		return false;
-    	}
+    	$checkResult = new \StdClass();
+    	$isActive = $this->isActive($quote ? $quote->getStoreId() : null);
+    	$checkResult->isAvailable = $isActive;
+    	$checkResult->isDeniedInConfig = !$isActive;
+    	// for future use in observers
+    	$this->_eventManager->dispatch(
+    			'payment_method_is_active',
+    			[
+    					'result' => $checkResult,
+    					'method_instance' => $this,
+    					'quote' => $quote
+    			]
+    			);
+    	$this->_logger->debug($checkResult->isAvailable);
+    	return $checkResult->isAvailable;
     }
 
-    /**
-     * Availability for currency
-     *
-     * @param string $currencyCode
-     * @return bool
-     */
-    public function canUseForCurrency($currencyCode)
-    {
-//     	$this->_logger->debug(__('canUseForCurrency'));
-//         if (!in_array($currencyCode, $this->_supportedCurrencyCodes)) {
-//             return false;
-//         }
-        return true;
-    }
 }
--- view/frontend/web/js/view/payment/method-renderer/seqr-payment-method.js
+++ view/frontend/web/js/view/payment/method-renderer/seqr-payment-method.js
@@ -6,19 +6,44 @@
 /*global define*/
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+     	'ko',
+        'Magento_Checkout/js/view/payment/default',
+        'Magento_Checkout/js/action/set-shipping-information',
+        'Magento_Checkout/js/model/step-navigator',
+        'Magento_Checkout/js/model/quote',
+        'SEQR_Plugin/js/view/payment/set-payment-method',
+        'SEQR_Plugin/js/view/payment/seqr-getinvoice'
     ],
-    function (Component) {
+    function (
+    		ko,
+    		Component,
+    		setShippingInformationAction,
+    		stepNavigator,
+    		quote,
+    		setPaymentMethodAction,
+    		seqrGetInvoice) {
         'use strict';
 
         return Component.extend({
             defaults: {
                 template: 'SEQR_Plugin/payment/seqr-payment'
             },
+            
 
             /** Returns send check to info */
             getMailingAddress: function() {
-                return window.checkoutConfig.payment.checkmo.mailingAddress;
+                return 'test';
+            },
+            
+            goToNextStep: function () {
+            	setPaymentMethodAction();
+                stepNavigator.next();
+                var test = seqrGetInvoice();
+//                alert (JSON.stringify(test));
+            },
+            
+            getCode: function () {
+                return this.item.method;
             },
 
            
